7608a9d9878fd31a2491a7d13e1706ba6a7ba4d9
Vaala <brice.bitot@neuf.fr>
2019-05-04 17:28:03 +0200

    make Controller

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,28 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', 'LoginController@showLoginForm')->name('login');
+Route::post('/', 'LoginController@login')->name('login');
+Route::get('logout', 'LoginController@logout')->name('logout');
+
+
+Route::get('a-propos', 'IndexController@propos')->name('propos');
+Route::get('inscription', 'IndexController@inscription')->name('inscription');
+
+
+Route::get('index', 'IndexController@index')->name('index');
+
+Route::get('cree-fiche', 'IndexController@cree_fiche')->name('crée une fiche');
+Route::post('cree-fiche', 'IndexController@cree_fiche')->name('crée une fiche');
+
+Route::get('fiche/{id}')->name('fiche');
+
+Route::get('forum', 'ForumController@index')->name('forum');
+Route::get('forum/post/{post}', 'ForumController@post')->name('post');
+
+Route::get('profil', 'IndexController@profil')->name('profil');
+
+Route::get('admin', 'AdminController@index')->name('admin');
+Route::get('admin/inscription', 'AdminController@inscription')->name('admin');
