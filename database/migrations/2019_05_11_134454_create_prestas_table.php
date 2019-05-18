596c2cd22b818a527fd2f174016ec823c0d11658
Vaala <brice.bitot@neuf.fr>
2019-05-18 11:10:32 +0200

    presta table

--- database/migrations/2019_05_11_134454_create_prestas_table.php
+++ database/migrations/2019_05_11_134454_create_prestas_table.php
@@ -15,6 +15,21 @@ class CreatePrestasTable extends Migration
     {
         Schema::create('prestas', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigIncrements('nigend');
+            $table->integer('id_type_repas')->unsigned();
+            $table->integer('id_type_prestas')->unsigned();
+            $table->string('nom_presta',100);
+            $table->decimal('latitude', 10, 8);
+            $table->decimal('longitude', 11, 8);
+            $table->string('num_portable', 10);
+            $table->string('num_fixe', 10);
+            $table->string('mail', 100);
+            $table->decimal('prix', 8,2);
+            $table->smallInteger('capacite');
+            $table->tinyInteger('livraison');
+            $table->char('lieu_de_conso',1);
+            $table->text('dispo');
+            $table->text('adresse');
             $table->timestamps();
         });
     }
